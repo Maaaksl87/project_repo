206968b8875c284213696a45824e2ef057c09cb3
Maaaksl87 <maaaksl87@gmail.com>
2024-06-21 00:12:39 +0300

    Task 027_2 CSS Modules

---
+++ task_027/component/src/components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Main.module.css";
+
+const Main = () => {
+  return (
+    <main className={styles.main}>
+      <h2>Створіть файли CSS з модульними стилями для кожного компонента.</h2>
+      <p>Наче правильно зрозумів завдання...</p>
+    </main>
+  );
+};
+
+export default Main;
