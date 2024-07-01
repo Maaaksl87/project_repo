84eba89818074ba2afdf9a67d7175505d19bea9e
Maaaksl87 <maaaksl87@gmail.com>
2024-07-01 15:49:57 +0300

    004_1

---
+++ Frontend_Pro/005/script.js
@@ -0,0 +1,6 @@
+const name = "Максим";
+const age = 18;
+let Student = (isStudent = true) ? "студент" : '';
+let height = 171.5;
+
+console.log(`Привіт, мене звати ${name}, мені ${age} років та я ${Student} Вінницького національного технічного університету. Мій зріст ${height} см`);
