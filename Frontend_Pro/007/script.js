4d69bb49ab15cd03853e5368514083552dfc712e
Maaaksl87 <maaaksl87@gmail.com>
2024-07-11 14:33:15 +0300

    007_2

--- Frontend_Pro/007/script.js
+++ Frontend_Pro/007/script.js
@@ -4,3 +4,9 @@ for (let i = 1; i <= 10; i++) {
     numbers.push(i)
 }
 console.log(numbers);
+
+
+//завдання 2
+let colors = ["червоний", "синій", "зелений", "жовтий", "чорний"];
+let findColor = (el) => el === "зелений";
+console.log(colors.findIndex(findColor)); //2
