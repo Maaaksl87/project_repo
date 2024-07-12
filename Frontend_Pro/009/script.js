00cefb1ced3e5b06371ed26812d0160a6d3f68c5
Maaaksl87 <maaaksl87@gmail.com>
2024-07-12 19:46:21 +0300

    009_2

--- Frontend_Pro/009/script.js
+++ Frontend_Pro/009/script.js
@@ -4,3 +4,14 @@ function calculateArea(width, height) {
 console.log(calculateArea(5, 10));
 console.log(calculateArea(3, 5));
 console.log(calculateArea(1, 8));
+
+
+
+let printMessage = (message) => console.log(message);
+
+
+function sendMessage(message, callback) {
+    callback(message);
+}
+
+sendMessage("hello, world?", printMessage); 
