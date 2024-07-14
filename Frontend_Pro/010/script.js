0d5955b90a54daa11b673b2bf627e80488d53a3a
Maaaksl87 <maaaksl87@gmail.com>
2024-07-14 18:02:40 +0300

    010_3

--- Frontend_Pro/010/script.js
+++ Frontend_Pro/010/script.js
@@ -24,4 +24,26 @@ function display(result) {
     console.log(result);
 }
 
-sum(display, 5, 6)
+sum(display, 5, 6);
+
+
+
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const success = true;
+
+        if (success) {
+            resolve("Promise виконується успішно");
+        } else {
+            reject("Promise відхиляється");
+        }
+    }, 2000);
+});
+
+myPromise
+    .then((message) => {
+        console.log(message);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
