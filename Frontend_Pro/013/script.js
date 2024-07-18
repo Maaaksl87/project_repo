7ec0b03a650d21c0a06e68b666f96888f0ee1abd
Maaaksl87 <maaaksl87@gmail.com>
2024-07-18 17:05:07 +0300

    013_2

--- Frontend_Pro/013/script.js
+++ Frontend_Pro/013/script.js
@@ -3,3 +3,12 @@ function multipyByTwo(num) {
 }
 
 console.log(multipyByTwo(5));
+
+console.log("Завдання 2");
+
+function findMax(arr) {
+    return Math.max(...arr);
+}
+
+const numbers = [10, 5, 8, 20, 3];
+console.log(findMax(numbers));
