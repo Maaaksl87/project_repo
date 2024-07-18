3764987d366890e71a96c09a4846450214b8a137
Maaaksl87 <maaaksl87@gmail.com>
2024-07-18 17:15:25 +0300

    013_3

--- Frontend_Pro/013/script.js
+++ Frontend_Pro/013/script.js
@@ -12,3 +12,17 @@ function findMax(arr) {
 
 const numbers = [10, 5, 8, 20, 3];
 console.log(findMax(numbers));
+
+console.log("Завдання 3");
+
+function updateUserInfo(key, val) {
+    return key.name = val;
+}
+
+function getUserInfo() {
+    return user.name
+}
+
+const user = { name: 'John', age: 30 };
+updateUserInfo(user, 'Alice');
+console.log(getUserInfo(user));
