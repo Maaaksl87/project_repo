b41731445cc2691789a59e7fd230fd69935377ff
Maaaksl87 <maaaksl87@gmail.com>
2024-07-19 15:27:09 +0300

    014_3

--- Frontend_Pro/014/script.js
+++ Frontend_Pro/014/script.js
@@ -7,3 +7,23 @@ console.log(filterArr([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
 function sumAllNum(arrnum) {
     return arrnum.reduce((acc, num) => acc + num, 0)
 } console.log(sumAllNum([1, 2, 3, 4, 5, 6, 7, 8,]));
+
+
+function replaceWords(input, target, replacement) {
+    const words = input.split(' ');
+    const replacedWords = words.map(word => {
+        if (word.toLowerCase() === target.toLowerCase()) {
+            return replacement;
+        }
+        return word;
+    });
+
+    return replacedWords.join(' ');
+}
+
+const input = "Тут має замінитися слово";
+const target = "слово";
+const replacement = "термін";
+
+const output = replaceWords(input, target, replacement);
+console.log(output);
