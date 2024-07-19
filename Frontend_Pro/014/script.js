8bac3f156825660a3419fe7ab549dae0698a0472
Maaaksl87 <maaaksl87@gmail.com>
2024-07-19 15:04:40 +0300

    014_2

--- Frontend_Pro/014/script.js
+++ Frontend_Pro/014/script.js
@@ -1,4 +1,9 @@
 function filterArr(arr) {
     return arr.filter(i => i % 2 === 0);
 }
-console.log(filterArr([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); 
+console.log(filterArr([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+
+
+function sumAllNum(arrnum) {
+    return arrnum.reduce((acc, num) => acc + num, 0)
+} console.log(sumAllNum([1, 2, 3, 4, 5, 6, 7, 8,]));
