4600186d98900730ed54d54a19f641555bf856a4
Maaaksl87 <maaaksl87@gmail.com>
2024-07-30 17:20:37 +0300

    016_2

--- Frontend_Pro/016/script.js
+++ Frontend_Pro/016/script.js
@@ -8,9 +8,22 @@ console.log(Object.getOwnPropertyDescriptor(person, "age"));
 
 Object.defineProperty(person, "gender", {
     value: "Woman",
-    writeable: false,
+    writable: false,
     enumerable: true,
     configurable: false,
 });
 
-console.log(person.gender);
+console.log(person.gender); // Woman
+console.log("--------------------------------");
+
+person.name = "Anna";
+
+console.log(person.name); //Anna,змінено успішно
+console.log("--------------------------------");
+
+for (key in person) {
+    console.log(key); //так, gender відображається. Якщо enumerable змінити на false, то не відображається.
+}
+console.log("--------------------------------");
+delete person.age; // видалилось успішно
+console.log(person);
