d968d8011af257038d1257f77860bac340d91d22
Maaaksl87 <maaaksl87@gmail.com>
2024-08-06 17:16:33 +0300

    018_6

--- Frontend_Pro/018/script.js
+++ Frontend_Pro/018/script.js
@@ -85,6 +85,7 @@ class Circle extends Shape {
 const obj = new Circle(10);
 console.log(obj.calculateArea());
 
+//ЗАВДАННЯ 5
 console.log("//////////////////////");
 
 class Library {
@@ -123,3 +124,145 @@ myLibrary.addBook(book2);
 
 console.log("Books in the library:");
 myLibrary.returnBooks().forEach(book => console.log(book.getInfo()));
+console.log('////////////////////////');
+
+
+//ЗАВДАННЯ 6
+/*
+Суть завдання:  систему управління університетом, яка буде включати інформацію про студентів,
+ викладачів та курси. Система повинна дозволяти додавати нових студентів та викладачів, 
+ призначати викладачів на курси, а також записувати студентів на курси.
+*/
+
+
+class Person_un {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+
+class Student_un extends Person_un {
+    constructor(firstName, lastName, age, courses = []) {
+        super(firstName, lastName, age);
+        this.courses = courses;
+    }
+
+    enrollCourse(course) {    // записує студента на курс
+        this.courses.push(course);
+    }
+}
+
+class Teacher extends Person_un {
+    constructor(firstName, lastName, age, courses = []) {
+        super(firstName, lastName, age);
+        this.courses = courses;
+    }
+
+    assignCourse(course) {    // призначає викладача на курс
+        this.courses.push(course);
+    }
+}
+
+class Course {
+    constructor(nameCourse) {
+        this.nameCourse = nameCourse;
+        this.teacher = null;
+        this.students = [];
+    }
+
+    addStudent(student) { // додає студента на курс
+        this.students.push(student);
+        student.enrollCourse(this);
+    }
+
+    setTeacher(teacher) { // призначає викладача на курс
+        this.teacher = teacher;
+        teacher.assignCourse(this);
+    }
+}
+
+class University {
+    constructor(students = [], teachers = [], courses = []) {
+        this.students = students;
+        this.teachers = teachers;
+        this.courses = courses;
+    }
+
+    addStudent(student) {
+        this.students.push(student);
+    }
+
+    addTeacher(teacher) {
+        this.teachers.push(teacher);
+    }
+
+    addCourse(course) {
+        this.courses.push(course);
+    }
+
+    enrollStudentInCourse(student, course) {
+        course.addStudent(student);
+    }
+
+    assignTeacherToCourse(teacher, course) {
+        course.setTeacher(teacher);
+    }
+}
+
+const university = new University();
+
+const student1 = new Student_un('John', 'Doe', 20);
+const student2 = new Student_un('Jane', 'Smith', 22);
+
+const teacher1 = new Teacher('Dr.', 'Brown', 45);
+const teacher2 = new Teacher('Prof.', 'Green', 50);
+
+const course1 = new Course('Mathematics');
+const course2 = new Course('Physics');
+
+university.addStudent(student1);
+university.addStudent(student2);
+
+university.addTeacher(teacher1);
+university.addTeacher(teacher2);
+
+university.addCourse(course1);
+university.addCourse(course2);
+
+university.enrollStudentInCourse(student1, course1);
+university.enrollStudentInCourse(student2, course2);
+
+university.assignTeacherToCourse(teacher1, course1);
+university.assignTeacherToCourse(teacher2, course2);
+
+console.log('Students:');
+university.students.forEach(student => {
+    console.log(student.getFullName());
+
+});
+
+console.log('--------------');
+
+console.log('Teachers:');
+university.teachers.forEach(teacher => {
+    console.log(teacher.getFullName());
+});
+
+console.log('--------------');
+
+console.log('Courses:');
+university.courses.forEach(course => {
+    console.log(`${course.nameCourse} - Teacher: ${course.teacher.getFullName()}`);
+    console.log('Students:');
+    course.students.forEach(student => {
+        console.log(student.getFullName());
+    });
+    console.log('--------------');
+});
+
