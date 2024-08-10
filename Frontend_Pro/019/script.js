8a11a4813b8a8c72afa31a672748ebf2cd9e7ac4
Maaaksl87 <maaaksl87@gmail.com>
2024-08-10 23:45:04 +0300

    019_2

--- Frontend_Pro/019/script.js
+++ Frontend_Pro/019/script.js
@@ -20,3 +20,32 @@ const task1 = Object.create(Task).constructor(1, 'Купити продукти'
 console.log(task1);
 console.log(task1.markAsDone());
 console.log(task1);
+
+console.log('//////////////');
+
+const TaskList = function (id, title, desc) {
+    this.id = id;
+    this.title = title;
+    this.desc = desc;
+    this.el = [];
+
+    TaskList.prototype.addToEl = function (item) {
+        this.el.push(item);
+        return this;
+    };
+};
+
+
+const newTask = new TaskList(1, 'Зробити першу справу', 'lorem.....');
+newTask.addToEl(123);
+newTask.addToEl('text');
+console.log(newTask);
+
+
+const secondTask = new TaskList(1, 'Зробити наступну справу', 'Кроки для вирішення...');
+secondTask.addToEl('елемент');
+console.log(secondTask);
+
+
+
+
