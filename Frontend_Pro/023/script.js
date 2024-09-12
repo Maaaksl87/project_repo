453ac36e51bc68730ec2ee54a83615d1ded392cf
Maaaksl87 <maaaksl87@gmail.com>
2024-09-12 15:55:56 +0300

    023_2

--- Frontend_Pro/023/script.js
+++ Frontend_Pro/023/script.js
@@ -2,3 +2,10 @@ document.getElementById('button').addEventListener('click', function () {
     let header = document.getElementById('header');
     header.textContent = 'Заголовок змінено!';
 });
+
+document.querySelector('.add-element-btn').addEventListener('click', function () {
+    let container = document.querySelector('.elements-container');
+    let newElement = document.createElement('p');
+    newElement.textContent = 'Новий елемент';
+    container.appendChild(newElement);
+});
