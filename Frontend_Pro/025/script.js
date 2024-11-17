76176a31861751dfddffd068a20d9dafebf25a08
Maaaksl87 <maaaksl87@gmail.com>
2024-11-17 18:12:11 +0200

    025_3

--- Frontend_Pro/025/script.js
+++ Frontend_Pro/025/script.js
@@ -26,3 +26,31 @@ function printNumbersTimeout(from, to) {
 }
 
 printNumbersTimeout(1, 5);
+
+///////////////////////////////// timer
+
+function countToNewYear() {
+    const targetDate = new Date('January 1, 2025 00:00:00').getTime();
+
+    function updateTimer() {
+        const now = new Date().getTime(); // поточний час
+        const timeLeft = targetDate - now;
+
+        if (timeLeft <= 0) {
+            console.log('З Новим Роком!');
+            return;
+        }
+        //кількість часу що залишилась
+        const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+        console.log(`Залишилось: ${days} дн. ${hours} год. ${minutes} хв. ${seconds} сек.`);
+
+
+        setTimeout(updateTimer, 1000);
+    }
+    updateTimer();
+}
+countToNewYear();
