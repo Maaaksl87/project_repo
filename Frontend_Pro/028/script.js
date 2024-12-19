a2b87bd8598f3f52575266701a2744792c1ca4c7
Maaaksl87 <maaaksl87@gmail.com>
2024-12-19 16:54:30 +0200

    028_2

--- Frontend_Pro/028/script.js
+++ Frontend_Pro/028/script.js
@@ -13,3 +13,38 @@ async function getUsers() {
     }
 }
 getUsers();
+
+/////////////////////////////////
+// 028_2
+
+/*
+function getData() {
+  return fetch('https://api.example.com/data')
+    .then(response => response.json())
+    .then(data => {
+      return fetch(`https://api.example.com/details/${data.id}`)
+        .then(detailsResponse => detailsResponse.json())
+        .then(details => {
+          console.log(details);
+        });
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+ */
+
+async function getData() {
+    try {
+        let response = await fetch('https://api.example.com/data');
+        let data = await response.json();
+
+        let detailsResponse = await fetch(`https://api.example.com/details/${data.id}`);
+        let details = await detailsResponse.json();
+
+        console.log(details);
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
