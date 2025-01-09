e0bfb42b005502037b8c1b8033fdf049ff9123f6
Maaaksl87 <maaaksl87@gmail.com>
2025-01-09 13:36:57 +0200

    033_1

---
+++ Frontend_Pro/033/script.js
@@ -0,0 +1,7 @@
+//1. Створення шаблонних рядків
+
+function introduce(name, age, city) {
+    return `Привіт, мене звати ${name}. Мені ${age} років і я живу в ${city}.`
+}
+
+console.log(introduce('Alex', 20, 'Lviv'))
