e438f71d3e8c3d527fd19075d34e03565459ce48
Maaaksl87 <maaaksl87@gmail.com>
2025-01-09 12:47:16 +0200

    032_2

--- Frontend_Pro/032/script.js
+++ Frontend_Pro/032/script.js
@@ -5,3 +5,15 @@ const arr2 = [4, 5, 6];
 const newArr = [...arr1, ...arr2, 7];
 
 console.log(newArr);
+
+//Завдання 2: Використання значень параметрів за замовчуванням та Rest параметрів
+
+function greetPeople(greeting = "Hello", ...names) {
+    return names.map(name => `${greeting}, ${name}`);
+}
+
+const greetings = greetPeople("Hi", "Alice", "Bob", "Charlie");
+console.log(greetings); // ["Hi, Alice", "Hi, Bob", "Hi, Charlie"]
+
+const defaultGreetings = greetPeople(undefined, "Alice", "Bob");
+console.log(defaultGreetings); // ["Hello, Alice", "Hello, Bob"]
