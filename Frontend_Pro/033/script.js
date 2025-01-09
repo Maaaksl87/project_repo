48dbcb116e707ea438189e42ba9f302beaa4774d
Maaaksl87 <maaaksl87@gmail.com>
2025-01-09 13:44:44 +0200

    032_2

--- Frontend_Pro/033/script.js
+++ Frontend_Pro/033/script.js
@@ -4,4 +4,13 @@ function introduce(name, age, city) {
     return `Привіт, мене звати ${name}. Мені ${age} років і я живу в ${city}.`
 }
 
-console.log(introduce('Alex', 20, 'Lviv'))
+console.log(introduce('Alex', 20, 'Lviv'));
+
+//2. Форматування багаторядкового тексту
+
+function shoppingList(items) {
+    return `Список покупок:\n${items.map(item => `- [${item}]`).join('\n')}`;
+}
+
+const items = ['item1', 'item2', 'item3'];
+console.log(shoppingList(items));
