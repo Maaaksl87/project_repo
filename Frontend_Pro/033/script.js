a56cdca2decb28f8a81121aa0479ed975b59d32c
Maaaksl87 <maaaksl87@gmail.com>
2025-01-09 14:26:40 +0200

    033_6

--- Frontend_Pro/033/script.js
+++ Frontend_Pro/033/script.js
@@ -26,3 +26,36 @@ const hobby = "coding";
 const result = highlight`Hi, I'm ${firstName} and I like ${hobby}.`;
 
 console.log(result);
+
+//4. Створення HTML шаблону
+
+function createCard(title, description) {
+    return `<div class="card">
+  <h2>${title}</h2>
+  <p>${description}</p>
+</div>`
+}
+
+console.log(createCard("Subject", "Lorem ipsum****"))
+
+//5. Динамічне формування URL
+
+function generateURL(protocol, domain, path) {
+    return `${protocol}://${domain}/${path}`
+}
+
+console.log(generateURL("https", "google.com", "#"))
+
+//6. Додаткове завдання (необов'язкове). Розширене тегування
+
+function emphasize(strings, ...values) {
+    let result = strings.map((str, i) => {
+        let value = values[i] ? `<em>${values[i]}</em>` : '';
+        return str + value;
+    }).join('');
+
+    result = result.replace("важливо", '<strong>важливо</strong>');
+    return result;
+}
+
+console.log(emphasize`Потрібно ${"постійно"} практикуватися! Це дуже важливо!`);
