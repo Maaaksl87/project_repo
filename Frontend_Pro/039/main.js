8eadb50d81d77db8e4e0d0fc8d8658914405550c
Maaaksl87 <maaaksl87@gmail.com>
2025-07-22 19:22:41 +0300

    task 4

--- Frontend_Pro/039/main.js
+++ Frontend_Pro/039/main.js
@@ -1,3 +1,7 @@
-import {capitalize} from "./stringUtils.js"
+import { capitalize } from "./stringUtils.js"
+import { getFirstElement } from "./arrayUtils.js"
+
+const myArray = [1, 2, 3, 4, 5];
+console.log(getFirstElement(myArray)); // Outputs: 1, works properly
 
 console.log(capitalize("any string"))
