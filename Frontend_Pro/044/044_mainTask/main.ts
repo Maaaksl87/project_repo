1e02e3c89ac24f84a8422314ddd663a53656daca
Maaaksl87 <maaaksl87@gmail.com>
2025-08-01 21:39:53 +0300

    044 - task 2

--- Frontend_Pro/044/044_mainTask/main.ts
+++ Frontend_Pro/044/044_mainTask/main.ts
@@ -1,9 +1,18 @@
-let userName: string = 'Max';
+
+// Завдання 1 - Типізація змінних та функцій
+
+let userName: string = "Max";
 let age: number = 19;
 let isStudent: boolean = true;
 
-const greet = (name:string) => {
-    return `Hello, ${name}`
+const greet = (name: string) => {
+  return `Hello, ${name}`;
 };
 
 console.log(greet("Max"));
+
+// Завдання 2 - Типізація масивів та кортежів
+
+let arrayOfNumbers: number[] = [1, 2, 3, 4, 5];
+let arrayOfFruits: string[] = ["apple", "banana", "cherry"];
+let person: [number, string] = [25, "John"]; //кортеж
