fd4de4fb60e7d1a2dc64236fedde5a19db3521d9
Maaaksl87 <maaaksl87@gmail.com>
2025-08-01 21:20:41 +0300

    044 - Завдання 1 - Типізація змінних та функцій

---
+++ Frontend_Pro/044/044_mainTask/main.ts
@@ -0,0 +1,9 @@
+let userName: string = 'Max';
+let age: number = 19;
+let isStudent: boolean = true;
+
+const greet = (name:string) => {
+    return `Hello, ${name}`
+};
+
+console.log(greet("Max"));
