37367d86a2220f38caf39a69e56c439c453e69d1
Maaaksl87 <maaaksl87@gmail.com>
2025-08-01 21:57:00 +0300

    044 - task 4

--- Frontend_Pro/044/044_mainTask/main.ts
+++ Frontend_Pro/044/044_mainTask/main.ts
@@ -1,4 +1,3 @@
-
 // Завдання 1 - Типізація змінних та функцій
 
 let userName: string = "Max";
@@ -20,14 +19,27 @@ let person: [number, string] = [25, "John"]; //кортеж
 // Завдання 3 - Створення інтерфейсу для об'єктів
 
 interface Person {
-    name: string;
-    age: number;
-    isStudent: boolean;
+  name: string;
+  age: number;
+  isStudent: boolean;
 }
 
 let user: Person = {
-    name: 'Max',
-    age: 19,
-    isStudent: true
-}
+  name: "Max",
+  age: 19,
+  isStudent: true,
+};
 console.log(user);
+
+// Завдання 4 - Типізація функцій та generics
+
+function addNumbers(argOne: number, argTwo: number): number {
+  return argOne + argTwo;
+}
+
+function indentify<T>(arg: T): T {
+    return arg;
+}
+
+let str = indentify<string>("This is a string");
+let num = indentify<number>(20);
