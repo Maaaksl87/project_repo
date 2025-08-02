f07ddf1154c14e8bd6e5494e43c2a60be3c958cf
Maaaksl87 <maaaksl87@gmail.com>
2025-08-02 21:26:33 +0300

    045 - Task 4

--- Frontend_Pro/045/dist/main.js
+++ Frontend_Pro/045/dist/main.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 let newUser = {
     id: 1,
-    name: 'Max',
+    name: "Max",
     email: "maaksl87@gmail.com",
     age: 20,
 };
@@ -12,7 +12,6 @@ function printInfo(newUser) {
     console.log(`ID: ${newUser.email}`);
     console.log(`ID: ${newUser.age}`);
 }
-;
 printInfo(newUser);
 function getStatus(status) {
     switch (status) {
@@ -26,9 +25,30 @@ function getStatus(status) {
 }
 console.log(getStatus("success"));
 // Завдання 3 - Робота з кортежами
-let infoAboutStudent = ['Jhon', 3.75];
+let infoAboutStudent = ["Jhon", 3.75];
 function getStudentInfo() {
     console.log(`Імя: ${infoAboutStudent[0]}, Середінй бал: ${infoAboutStudent[1]}`);
 }
 getStudentInfo();
+// Завдання 4 - Enum для статусів
+var OrderStatus;
+(function (OrderStatus) {
+    OrderStatus[OrderStatus["Pending"] = 0] = "Pending";
+    OrderStatus[OrderStatus["Shipped"] = 1] = "Shipped";
+    OrderStatus[OrderStatus["Delivered"] = 2] = "Delivered";
+    OrderStatus[OrderStatus["Cancelled"] = 3] = "Cancelled";
+})(OrderStatus || (OrderStatus = {}));
+function getOrderStatus(status) {
+    switch (status) {
+        case OrderStatus.Pending:
+            return "очікуйте!";
+        case OrderStatus.Shipped:
+            return "Відправлено";
+        case OrderStatus.Delivered:
+            return "Смачного";
+        case OrderStatus.Cancelled:
+            return "Скасовано :(";
+    }
+}
+console.log(getOrderStatus(OrderStatus.Delivered));
 //# sourceMappingURL=main.js.map
--- Frontend_Pro/045/main.ts
+++ Frontend_Pro/045/main.ts
@@ -7,41 +7,67 @@ type User = {
 };
 
 let newUser: User = {
-    id: 1,
-    name: 'Max',
-    email: "maaksl87@gmail.com",
-    age: 20,
+  id: 1,
+  name: "Max",
+  email: "maaksl87@gmail.com",
+  age: 20,
 };
 
-function printInfo (newUser: User): void {
-    console.log(`ID: ${newUser.id}`);
-    console.log(`ID: ${newUser.name}`);
-    console.log(`ID: ${newUser.email}`);
-    console.log(`ID: ${newUser.age}`);
-};
+function printInfo(newUser: User): void {
+  console.log(`ID: ${newUser.id}`);
+  console.log(`ID: ${newUser.name}`);
+  console.log(`ID: ${newUser.email}`);
+  console.log(`ID: ${newUser.age}`);
+}
 
 printInfo(newUser);
 
 // Завдання 2 - Літеральні типи
 
 type Status = "success" | "error" | "pending";
-function getStatus (status: Status): string {
-    switch (status) {
-        case "success":
-            return "Успішно!";
-        case "error":
-            return "Помилка!";
-        case "pending":
-            return "Очікування!";
-    }
+function getStatus(status: Status): string {
+  switch (status) {
+    case "success":
+      return "Успішно!";
+    case "error":
+      return "Помилка!";
+    case "pending":
+      return "Очікування!";
+  }
 }
 console.log(getStatus("success"));
 
 // Завдання 3 - Робота з кортежами
 
-let infoAboutStudent: [string, number] = ['Jhon', 3.75];
+let infoAboutStudent: [string, number] = ["Jhon", 3.75];
 
 function getStudentInfo(): void {
-    console.log(`Імя: ${infoAboutStudent[0]}, Середінй бал: ${infoAboutStudent[1]}`)
+  console.log(
+    `Імя: ${infoAboutStudent[0]}, Середінй бал: ${infoAboutStudent[1]}`
+  );
 }
 getStudentInfo();
+
+// Завдання 4 - Enum для статусів
+
+enum OrderStatus {
+  Pending,
+  Shipped,
+  Delivered,
+  Cancelled,
+}
+
+function getOrderStatus(status: OrderStatus): string {
+  switch (status) {
+    case OrderStatus.Pending:
+      return "очікуйте!";
+    case OrderStatus.Shipped:
+      return "Відправлено";
+    case OrderStatus.Delivered:
+      return "Смачного";
+    case OrderStatus.Cancelled:
+      return "Скасовано :(";
+  }
+}
+
+console.log(getOrderStatus(OrderStatus.Delivered));
