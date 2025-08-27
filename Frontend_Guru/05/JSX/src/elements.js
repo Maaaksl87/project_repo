c80bfa2fb65c6b49c86b114ac16a0c13d573f10e
Maaaksl87 <maaaksl87@gmail.com>
2025-08-27 18:13:49 +0300

    Task 2 та 3

--- Frontend_Guru/05/JSX/src/elements.js
+++ Frontend_Guru/05/JSX/src/elements.js
@@ -1,2 +1,10 @@
 // Завдання 1 - Створення простого JSX елементу
 const welcomeElement = <h1>Hello, world!</h1>
+
+// Завдання 2 - Інтерполяція та JSX
+let userAge = 20;
+const welcomeOtherElement = <p>Привіт! Тобі {userAge} років!</p>
+
+// Завдання 3 - Перевірка на відвідувача
+const isVisitor = true;
+const visitorStatusElement = (isVisitor) ? <p>Ласкаво просимо на наш курс!</p> : <p>Будь ласка, зареєструйтесь для доступу</p>
