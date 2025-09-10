f7cc15a8505c45620d046e76f0c9430dd6550922
Maaaksl87 <maaaksl87@gmail.com>
2025-09-11 00:39:37 +0300

    010 task 2

--- Frontend_Guru/010/PFC/src/App.jsx
+++ Frontend_Guru/010/PFC/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import TaskList from "./components/TaskList";
 import ReactMemoList from "./components/ReactMemoList";
+import { StatelessComponent } from "./components/StatelessComponent";
 
 function App() {
   return (
     <>
       <TaskList />
       <ReactMemoList />
+      <StatelessComponent name="Artur" age={22} />
     </>
   );
 }
---
+++ Frontend_Guru/010/PFC/src/components/StatelessComponent.jsx
@@ -0,0 +1,7 @@
+export const StatelessComponent = ({ name, age }) => {
+  return (
+    <h2>
+      Привіт, {name} тобі уже {age} років
+    </h2>
+  );
+};
